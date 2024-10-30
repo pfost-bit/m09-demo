17bda03be5cda66b9c21f4b7eb8ebe0f333c595d
Patrick_Foster <ezq9qu.virginia.edu>
2024-10-29 20:18:12 -0400

    First

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='https://github.com/pfost-bit/m09-demo',
+    author='Pat Foster',
+    author_email='ezq9qu@virginia.edu',
+    license='MIT',
+    packages=['demo']
+)
